7cb00d33594cf44bf52019a4c7e71c5f8863ec13
Andrew Willmott <awillmott+git@gmail.com>
2019-04-01 17:14:04 +0100

    formatting updates

--- Bezier2.h
+++ Bezier2.h
@@ -16,16 +16,16 @@
 
 const float kBezierError2[3][3] = 
 {
-    {     0.2f,     0.1f, 0.033333f },
-    {     0.1f, 0.13333f,     0.1f },
-    { 0.033333f,     0.1f,     0.2f }
+    { 0.20000f, 0.10000f, 0.03333f },
+    { 0.10000f, 0.13333f, 0.10000f },
+    { 0.03333f, 0.10000f, 0.20000f }
 };
 
 // Matrix for converting integrals v order-2 Bezier basis functions to corresponding Bezier coefficients
 
 const float kToBezier2[3][3] = 
 {
-    {       9f,      -9f,       3f },
-    {      -9f,      21f,      -9f },
-    {       3f,      -9f,       9f }
+    {    9.00f,   -9.00f,    3.00f },
+    {   -9.00f,   21.00f,   -9.00f },
+    {    3.00f,   -9.00f,    9.00f }
 };
